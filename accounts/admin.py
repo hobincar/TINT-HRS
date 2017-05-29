1642293d2f74d0f4e09ae3679e195e977de9459c
Hobin Ryu <hobincar@kaist.ac.kr>
2017-05-29 14:08:51 +0900

    Admin can manage user's reservation

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import User
+from reservations.models import Reservation
 
-admin.site.register(User)
+
+class ReservationInline(admin.TabularInline):
+    model = Reservation
+    fk_name = 'customer'
+    extra = 3
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    inlines = (ReservationInline,)
+
+
+admin.site.register(User, UserAdmin)
