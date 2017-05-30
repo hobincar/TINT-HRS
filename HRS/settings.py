584fd4c086ce7135dbfa6d4d64c23becebe599f3
Hobin Ryu <hobincar@kaist.ac.kr>
2017-05-31 00:00:02 +0900

    Modify setting for heroku

--- HRS/settings.py
+++ HRS/settings.py
@@ -134,11 +134,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/static/'
+#STATIC_ROOT = '/static/'
 MEDIA_ROOT = '/static/image_upload/'
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 LOGIN_REDIRECT_URL = '/'
 
