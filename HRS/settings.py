73b51e82233342c1ad9c0d5f65649691137b2631
Hobin Ryu <hobincar@kaist.ac.kr>
2017-05-31 00:33:12 +0900

    Modify setting for heroku

--- HRS/settings.py
+++ HRS/settings.py
@@ -133,12 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles"),
+]
 MEDIA_ROOT = '/static/image_upload/'
-#STATICFILES_DIRS = (
-#    'static',
-#)
 
 LOGIN_REDIRECT_URL = '/'
 
